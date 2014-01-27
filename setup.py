0daacdcfd0a34af5b394eb53e83344ec98cc48d0
daveberg <david.r.bisset@gmail.com>
2014-01-27 15:58:24 +0000

    some setup changes

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ requires = [
 
 
 setup(
-    name='nosetest webdriver failure screen shots',
-    version='0.13',
+    name='nose_webdriverfailuress',
+    version='0.14',
     author='Dave',
     author_email='david.bisset@fanduel.com',
     description='capture screenshot on test failure or error',
-    py_modules=['failurescreenshots'],
+    py_modules=['nose_webdriverfailuress'],
     include_package_data=True,
     entry_points={
         'nose.plugins.0.10': [
-            'failurescreenshots = failurescreenshots:FailureScreenshots'
+            'nose_webdriverfailuress = nose_webdriverfailuress:FailureScreenshots'
         ]
     },
     install_requires=requires
