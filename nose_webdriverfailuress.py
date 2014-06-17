670771a88b62831f23077a9f91326105386fbae4
daveberg <david.r.bisset@gmail.com>
2014-06-17 10:59:11 +0100

    Handle no driver attribute
    Lazily - log that it was absent, do nothing useful

--- nose_webdriverfailuress.py
+++ nose_webdriverfailuress.py
@@ -1,29 +1,11 @@
 """Attempt to capture Webdriver screenshots on test error or failure.
 Requires an active driver instance named driver within the test
 """
+import logging
 import os
 
 from selenium.common.exceptions import WebDriverException
 from nose.plugins.base import Plugin
-
-
-def get_screen_shot(driver, test_id):
-    # don't care about leading 'tests' bit, they're all tests
-    # needs revisiting if tests get moved to sub-folders
-    name = test_id.split('.', 1)[1]
-    # make a folder to keep them in
-    path = 'screenshots'
-    try:
-        os.makedirs(path)
-    except OSError:
-        if not os.path.isdir(path):
-            raise
-    # save to it
-    try:
-        driver.get_screenshot_as_file('{path}/{name}.png'.format(path=path, name=name))
-    except WebDriverException as e:
-        # possibly browser has died or communication lost but not worth crashing for
-        print('Failed to take screenshot: {error}'.format(error=e))
 
 
 class FailureScreenshots(Plugin):
@@ -31,10 +13,10 @@ class FailureScreenshots(Plugin):
     Requires an active driver instance named driver within the test
     """
     name = 'failurescreenshots'
-    score = 2  # run when?
     enabled = True
     enabled_for_errors = True
     enabled_for_failures = True
+    log = logging.getLogger('nose.plugins.failure_screenshots')
 
     def __init__(self):
         super(FailureScreenshots, self).__init__()
@@ -43,11 +25,11 @@ class FailureScreenshots(Plugin):
         """Register commandline options."""
         Plugin.options(self, parser, env)
         parser.add_option(
-            "--ss-errors-only", action="store_true", dest="debugErrors", default=False,
-            help="Screenshot errors but not failures")
+            "--ss-errors-only", action="store_true", dest="debugErrors",
+            default=False, help="Screenshot errors but not failures")
         parser.add_option(
-            "--ss-failures-only", action="store_true", dest="debugFailures", default=False,
-            help="Screenshot failures but not errors")
+            "--ss-failures-only", action="store_true", dest="debugFailures",
+            default=False, help="Screenshot failures but not errors")
 
     def configure(self, options, config):
         """Configure which kinds of failures trigger plugin."""
@@ -59,15 +41,35 @@ class FailureScreenshots(Plugin):
             self.enabled_for_errors = not options.debugFailures
             self.enabled_for_failures = not options.debugErrors
 
+    def get_screen_shot(self, test, test_id):
+        # don't care about leading 'tests' bit, they're all tests
+        # needs revisiting if tests get moved to sub-folders
+        name = test_id.split('.', 1)[1]
+        # make a folder to keep them in
+        path = 'screenshots'
+        try:
+            os.makedirs(path)
+        except OSError:
+            if not os.path.isdir(path):
+                raise
+        # save to it
+        try:
+            test.driver.get_screenshot_as_file('{path}/{name}.png'.format(path=path, name=name))
+        except AttributeError as e:
+            # No WebDriver called driver? Oops.
+            self.log.warning('No driver instance found: {error}'.format(error=e))
+        except WebDriverException as e:
+            # possibly browser died or communication lost, not worth crashing
+            self.log.warning('Failed to take screenshot: {error}'.format(error=e))
+
     def addError(self, test, err):
         """Something."""
         if not self.enabled_for_errors:
             return
-        get_screen_shot(test.test.driver, test.id())
+        self.get_screen_shot(test.test, test.id())
 
     def addFailure(self, test, err):
         """Something."""
         if not self.enabled_for_failures:
             return
-        get_screen_shot(test.test.driver, test.id())
-
+        self.get_screen_shot(test.test, test.id())
